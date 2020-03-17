5a0a08be95cc67f043c569d10e465222605524e5
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-17 19:48:49 +0200

    input field as a separate component + props

--- sources/app/resources/js/components/InputField.vue
+++ sources/app/resources/js/components/InputField.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="relative pb-4">
-        <label for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">Contact Name</label>
-        <input id="name" type="text" class="pt-8 text-gray-900 w-full border-b pb-2 focus:outline-none focus:border-blue-400" placeholder="Contact Name">
+        <label :for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">{{label}}</label>
+        <input :id="name" type="text" class="pt-8 text-gray-900 w-full border-b pb-2 focus:outline-none focus:border-blue-400" :placeholder="placeholder">
     </div>
 </template>
 
 <script>
     export default {
-        name: "InputField"
+        name: "InputField",
+        props: [
+            'name', 'label', 'placeholder'
+        ]
     }
 </script>
 
--- sources/app/resources/js/views/ContactsCreate.vue
+++ sources/app/resources/js/views/ContactsCreate.vue
@@ -1,19 +1,7 @@
 <template>
     <div>
         <form>
-            <InputField></InputField>
-            <div class="relative pb-4">
-                <label for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">Contact Name</label>
-                <input id="name" type="text" class="pt-8 text-gray-900 w-full border-b pb-2 focus:outline-none focus:border-blue-400" placeholder="Contact Name">
-            </div>
-            <div class="relative pb-4">
-                <label for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">Contact Name</label>
-                <input id="name" type="text" class="pt-8 text-gray-900 w-full border-b pb-2 focus:outline-none focus:border-blue-400" placeholder="Contact Name">
-            </div>
-            <div class="relative pb-4">
-                <label for="name" class="text-blue-500 pt-2 uppercase text-xs font-bold absolute">Contact Name</label>
-                <input id="name" type="text" class="pt-8 text-gray-900 w-full border-b pb-2 focus:outline-none focus:border-blue-400" placeholder="Contact Name">
-            </div>
+            <InputField name="name" label="Contact Name" placeholder="Contact Name" />
 
             <div class="flex justify-end mb-4 pb-2">
                 <button class="rounded-full py-2 px-4 hover:border-red-500 border border-gray-400 text-red-500 mr-2">Cancel</button>
